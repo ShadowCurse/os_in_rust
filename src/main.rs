2c5ac4f9ddbea95e278d1aa63e6605bf111d562d
ShadowCurse <shadowcurse57@gmail.com>
2022-08-10 20:07:33 +0300

    basic bootable image

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -8,8 +9,16 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(main);
+
+fn main(boot_info: &'static mut BootInfo) -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        for byte in framebuffer.buffer_mut() {
+            *byte = 0x90;
+        }
+    }
+
     loop {}
 }
-
