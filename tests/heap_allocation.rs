2d4754fbb4b0c2edd5d834165a53287e366b1fb4
ShadowCurse <shadowcurse57@gmail.com>
2022-10-20 21:31:53 +0300

    heap allocation test

---
+++ tests/heap_allocation.rs
@@ -0,0 +1,66 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(os_in_rust::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
+
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
+use os_in_rust::{
+    alloc::{boxed::Box, vec::Vec},
+    allocator::{init_heap, HEAP_SIZE},
+    init,
+    memory::{self, BootInfoFrameAllocator},
+    println,
+};
+use x86_64::VirtAddr;
+
+entry_point!(main);
+fn main(boot_info: &'static mut BootInfo) -> ! {
+    init();
+
+    let phys_mem_offset = VirtAddr::new(*boot_info.physical_memory_offset.as_ref().unwrap());
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::new(&boot_info.memory_regions) };
+    init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    test_main();
+    panic!("Qemu not exited");
+}
+
+#[test_case]
+fn simple_allocation() {
+    let heap_value_1 = Box::new(41);
+    let heap_value_2 = Box::new(13);
+    assert_eq!(*heap_value_1, 41);
+    assert_eq!(*heap_value_2, 13);
+}
+
+#[test_case]
+fn large_vec() {
+    let n = 1000;
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+    assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
+}
+
+#[test_case]
+fn many_boxes() {
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
+}
+
+#[panic_handler]
+fn ph(info: &PanicInfo) -> ! {
+    use os_in_rust::{exit_qemu, QemuExitCode};
+
+    println!("[failed]\n");
+    println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
